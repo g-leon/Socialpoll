16b3cf9a723a3d6aac0814365735bad288dcc11c
Leon Guianu <leon.guianu@avira.com>
2017-03-17 17:08:37 +0200

    Add authorization mechanism

--- twittervotes/twitter.go
+++ twittervotes/twitter.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"io"
 	"net"
 	"time"
-	"io"
+	"log"
+	"github.com/joeshaw/envdecode"
+	"github.com/garyburd/go-oauth/oauth"
 )
 
-var conn net.Conn
-var reader io.ReadCloser
+var (
+	conn       net.Conn
+	reader     io.ReadCloser
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
 
 // dial ensures that the connection is closed and
 // then opens a new connection, keeping the conn
@@ -39,6 +46,31 @@ func closeConn() {
 	}
 	if reader != nil {
 		reader.Close()
+	}
+}
+
+// setupTwitterAuth reads the environment variables and
+// sets up the OAuth object needed in order to
+// authenticate requests.
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY, required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET, required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN, required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET, required"`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+	creds = &oauth.Credentials{
+		Token: ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token: ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
 	}
 }
 
