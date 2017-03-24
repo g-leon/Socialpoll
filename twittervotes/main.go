aba643f6084bd32817a6a4bc65daa154e70e62a3
Leon Guianu <leon.guianu@avira.com>
2017-03-24 13:57:31 +0200

    Add methods for interacting with mongo

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
+
+var db *mgo.Session
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+
+func dialdb() error {
+	var err error
+	log.Println("dialing mongodb: localhost")
+	// TODO read connection string from config
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("closed database connection")
+}
+
 func main() {
 
 }
