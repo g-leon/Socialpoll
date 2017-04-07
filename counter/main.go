cfc59be1a5c2db9ddbf0e8e2561bf1b34581b7a2
Leon Guianu <leon.guianu@avira.com>
2017-04-07 13:48:08 +0300

    Add votes handler

--- counter/main.go
+++ counter/main.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"flag"
 	"os"
+	"log"
+	"gopkg.in/mgo.v2"
+	"sync"
+	"github.com/bitly/go-nsq"
 )
 
-var fatalErr error
+var (
+	fatalErr error
+	counts map[string]int
+	countsLock sync.Mutex
+)
 
 func fatal(e error) {
 	fmt.Println(e)
@@ -20,6 +28,44 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+
+	log.Println("Connecting to the database...")
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	defer func() {
+		log.Println("Closing database connection...")
+		db.Close()
+	}()
+	pollData := db.DB("ballots").C("polls")
+
+	log.Println("Connection to NSQ...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	// votes handler
+	q.AddHandler(nsq.HandlerFunc(func (m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	if err := q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
+
 }
 
 
