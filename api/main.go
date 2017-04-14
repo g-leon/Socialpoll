c67c1807940964f91bcea597667e5b3a167b3e50
Leon Guianu <leon.guianu@avira.com>
2017-04-14 14:52:48 +0300

    Add API key management functions

--- api/main.go
+++ api/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "context"
+import (
+	"context"
+	"net/http"
+	"gopkg.in/mgo.v2"
+)
+
+// Server is the API server.
+// Server makes sure handlers will not make
+// database management mistakes.
+type Server struct {
+	db *mgo.Session
+}
 
 // contextKey helps to create uniform keys for
 // context.Context, where keys are of type interface{}
@@ -10,6 +21,10 @@ type contextKey struct {
 
 var contextKeyAPIKey = &contextKey{"api-key"}
 
+func main() {
+
+}
+
 // APIKey is a helper function that, given a context,
 // extracts a key.
 func APIKey(ctx context.Context) (string, bool) {
@@ -17,6 +32,32 @@ func APIKey(ctx context.Context) (string, bool) {
 	return key, ok
 }
 
-func main() {
+// withAPIKey is a wrapper of a HandlerFunc that helps with
+// asking clients to provide an API key which facilitates the
+// implementation of user authentication and authorisation.
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		if !isValidAPIKey(key) {
+			respondErr(w, r, http.StatusUnauthorized, "invalid API key")
+			return
+		}
+		ctx := context.WithValue(r.Context(), contextKeyAPIKey, key)
+		fn(w, r.WithContext(ctx))
+	}
+}
 
+func isValidAPIKey(key string) bool {
+	// TODO check given key against a value read from a config file or database
+	return key == "abc123"
+}
+
+// withCORS let's one to circumnavigate the same-origin policy,
+// by being able to serve websites hosted on other domains as well.
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
 }
